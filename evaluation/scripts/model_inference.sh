33198b2dd7e42dcdeffb3ec7ac29f2b6cf5ce837
leejungho1234 <leejungho@lguplus.co.kr>
2024-11-20 04:50:42 +0000

    modify model inference.sh

--- evaluation/scripts/model_inference.sh
+++ evaluation/scripts/model_inference.sh
@@ -1,7 +1,7 @@
 for PROMPT_ID in {1..5}
 do
-python3 2_model_inference.py \
-    --data-path data/KoBBQ_all/KoBBQ_all_evaluation_$PROMPT_ID.json \
-    --output-dir outputs/raw/KoBBQ_all_$PROMPT_ID \
+python3 ../2_model_inference.py \
+    --data-path ../data/KoBBQ_all/KoBBQ_all_evaluation_$PROMPT_ID.json \
+    --output-dir ../outputs/raw/KoBBQ_all_$PROMPT_ID \
     --model-name EXAONE-3-INST
 done
