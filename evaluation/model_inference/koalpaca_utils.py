4280b1af2867e547a20103731f6125ee145008f2
leejungho1234 <leejungho@lguplus.co.kr>
2024-11-20 06:40:06 +0000

    temp_commit

--- evaluation/model_inference/koalpaca_utils.py
+++ evaluation/model_inference/koalpaca_utils.py
@@ -6,17 +6,18 @@ MIT license
 
 import torch
 from tqdm.auto import tqdm
-from transformers import pipeline
+from transformers import pipeline, AutoTokenizer
 
 KOALPACA_MODEL = ['EXAONE-3-INST']
 KOALPACA_MODEL_PATH = {'EXAONE-3-INST':'LGAI-EXAONE/EXAONE-3.0-7.8B-Instruct'}
 
 def load_koalpaca(model_name='EXAONE-3-INST'):
+    tokenizer = AutoTokenizer.from_pretrained(KOALPACA_MODEL_PATH[model_name])
     pipe = pipeline(
         'text-generation', 
         torch_dtype=torch.bfloat16, 
         model=KOALPACA_MODEL_PATH[model_name], 
-        # tokenizer=KOALPACA_MODEL_PATH[model_name],
+        tokenizer=tokenizer,
         device_map="auto", 
         trust_remote_code=True)
     # pipe.model.config.pad_token_id = pipe.model.config.eos_token_id
@@ -31,11 +32,11 @@ def get_koalpaca_response(
     batch_size
 ):
     assert model_name in KOALPACA_MODEL
-    
+    prompt = [[{'role':'user', 'content':p}] for p in prompt]
     result = []
-    breakpoint()
+    
     try:
-        for idx, out in enumerate(tqdm(pipe([{'role':'user', 'content':prompt}], batch_size=batch_size, max_new_tokens=max_tokens), total=len(prompt))):
+        for idx, out in enumerate(tqdm(pipe(prompt, batch_size=batch_size, max_new_tokens=max_tokens), total=len(prompt))):
             raw = out[0]['generated_text']
             result.append(raw.split(prompt[idx])[-1])
     except Exception as e:
