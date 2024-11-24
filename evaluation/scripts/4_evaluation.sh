16367b4555ae04b36eb2513abe684bbe56dec72f
root <leejungho@lguplus.co.kr>
2024-11-24 22:53:05 +0000

    temp_commit

---
+++ evaluation/scripts/4_evaluation.sh
@@ -0,0 +1,13 @@
+MODELS='EXAONE-3-INST'
+
+for PROMPT_ID in {1..5}
+do
+python3 5_evaluation.py \
+    --evaluation-result-path ../evaluation_result/KoBBQ_all_$PROMPT_ID.tsv \
+    --model-result-tsv-dir ../outputs/processed/KoBBQ_all_$PROMPT_ID \
+    --topic KoBBQ_all_evaluation \
+    --test-or-all all \
+    --prompt-tsv-path 0_evaluation_prompts.tsv \
+    --prompt-id $PROMPT_ID \
+    --models $MODELS
+done
